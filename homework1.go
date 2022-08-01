96c093ee28ebbe1341df4d9a782053e55cd4b637
akadir8 <109678256+akadir8@users.noreply.github.com>
2022-08-02 02:27:39 +0300

    Update homework1.go

--- homework1.go
+++ homework1.go
@@ -4,26 +4,26 @@ import "fmt"
 
 func main() {
 
-	firstExam := 98
-	secondExam := 80
+	Firstexam := 98
+	Secondexam := 80
 	//TODO
 	//golang dilinde değişken ve fonksiyon isimlerinin büyük harfle başlaması özel bir durumdur
 	// camelCase yöntemi en ideal kullanım biçimidir
 	// tüm değişken ve fonksiyon isimlerinde camelCase yöntemini uygulayalım
-	Average := takeTheAverage(firstExam, secondExam)
-	receivedDocument(Average)
+	Average := TaketheAverage(Firstexam, Secondexam)
+	Receivedocument(Average)
 	println(Average)
 }
 
-func takeTheAverage(firstExam int, secondExam int) int {
+func TaketheAverage(Firstexam int, Secondexam int) int {
 
-	return (firstExam + secondExam) / 2
+	return (Firstexam + Secondexam) / 2
 
 }
-func receivedDocument(gradeAverage int) {
-	if gradeAverage >= 85 {
+func Receiveddocument(Gradeaverage int) {
+	if Gradeaverage >= 85 {
 		fmt.Println("You have earned a certificate of appreciation")
-	} else if gradeAverage >= 70 {
+	} else if Gradeaverage >= 70 {
 		fmt.Println("You have earned a certificate of thank")
 	} else {
 		fmt.Println("You did not qualify for the document")
